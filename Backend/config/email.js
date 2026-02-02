171cdf5dcc8e5bf7df6a855cb4eb4d211b25791f
Bhavesh-malvi <143591639+Bhavesh-malvi@users.noreply.github.com>
2026-02-02 14:15:38 +0530

    feat: add email sending utility using nodemailer.

--- Backend/config/email.js
+++ Backend/config/email.js
@@ -4,14 +4,11 @@ const sendMail = async ({ email, subject, message }) => {
 
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true, // SSL
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-      },
-      family: 4 // Force IPv4
+      }
     });
 
 
